f97acdc840f865261005edaaad21b56ab1f57ec3
zheyeah <zheyeah@gmail.com>
2018-01-24 16:17:37 -0800

    Modify appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-     render html:"hello,world,toy_app!"
+     render html:"hello,world,toy_app,I did it!"
   end
 end
